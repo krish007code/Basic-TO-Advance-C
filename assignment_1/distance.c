c44662d6196892556585922236b657e4c4c6150e
Krish007code <kavyanshkumarbaghel@gmail.com>
2025-10-20 23:28:56 +0530

    Write program, which reads a, b, c, and d and finds distance between points (a, b) and(c.d)

--- assignment_1/distance.c
+++ assignment_1/distance.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     int a, b, c, d;
+    printf("Enter a : ");
+    scanf("%i", &a);
+    printf("Enter b : ");
+    scanf("%i", &b);
+    printf("Enter c : ");
+    scanf("%i", &c);
+    printf("Enter d : ");
+    scanf("%i", &d);
+    float distance = sqrt( pow((c - a),2) + pow((d - b),2));
+    printf("%f", distance);
+    return 0;
 }
